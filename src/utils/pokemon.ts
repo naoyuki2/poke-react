cba1e8d7cbc9388a4c52e67c4bb046359f53f3dd
naoyuki <kamaboko912@gmail.com>
2024-01-07 15:29:42 +0900

    reject削除

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,7 +1,7 @@
 import { NamedAPIResource, NamedAPIResourceList } from 'pokenode-ts'
 
 export const getAllPokemon = (url: string): Promise<NamedAPIResourceList> => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         //Promise(約束)下の処理が終わったらresolve(解決)を返す
         fetch(url)
             .then((res) => res.json())
@@ -12,7 +12,7 @@ export const getAllPokemon = (url: string): Promise<NamedAPIResourceList> => {
 }
 
 export const getPokemon = (url: string): Promise<NamedAPIResource> => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         fetch(url)
             .then((res) => res.json())
             .then((data) => {
